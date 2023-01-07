75f89ded39365dbb6eb7231bfc87e434052542c6
Jakub Jamroz <jakubjamroz23@gmail.com>
2023-01-07 13:16:03 +0100

    EXP-1 preliminary setup

---
+++ src/components/navBar/NavBar.tsx
@@ -0,0 +1,12 @@
+import { AppBarContainer, Expensify } from "./navBar.style";
+
+const NavBar = () => {
+  return (
+    <AppBarContainer>
+      <Expensify>Expensify</Expensify>
+      <div>Login</div>
+    </AppBarContainer>
+  );
+};
+
+export default NavBar;
---
+++ src/globalStyles.ts
@@ -0,0 +1,42 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+*,
+*:after,
+*:before {
+    box-sizing: border-box;
+    margin: 0 auto;
+    padding: 0;
+}
+
+html, body {
+  overflow-x: hidden;
+  overflow-y: hidden;
+  width: 100%;
+  height: 100%;
+}
+
+html {
+  font-size: 62.5%;
+  background-color: #f7f7f7;
+}
+
+body {
+  position: relative;
+  z-index: 0;
+  min-height: 100%;
+  color: #000;
+  margin: 0;
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+    'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+    sans-serif;
+  font-size: 1.6rem;
+  font-weight: 400;
+  font-style: normal;
+  line-height: normal;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+
+
+`;
