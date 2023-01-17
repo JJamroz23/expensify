c25547c18d5979665e25ae61f4b03867345fc2ab
Jakub Jamroz <jakubjamroz23@gmail.com>
2023-01-17 12:51:33 +0100

    app env key

--- src/utils/firebase/index.ts
+++ src/utils/firebase/index.ts
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAcBILYEK6gFI33kZUyilae4oGle9U0fu8",
-  authDomain: "exepnsify-f3ea1.firebaseapp.com",
-  projectId: "exepnsify-f3ea1",
-  storageBucket: "exepnsify-f3ea1.appspot.com",
-  messagingSenderId: "817574760616",
-  appId: "1:817574760616:web:919f187061734199f6f29d",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 initializeApp(firebaseConfig);
